8bf93dfe70574ff840417cfa23f139663118d605
北渔 <ruiting.xrt@alibaba-inc.com>
2018-07-29 17:56:54 +0800

    + 查看环境列表链接和快捷键

--- src/content_scripts/inject.ts
+++
@@ -1,8 +0,0 @@
-import { pageInfoEnum, requestEnum } from "../consts";
-
-(function(win, doc) {
-  win.postMessage(
-    { type: requestEnum.type, text: win[pageInfoEnum.debug] },
-    "*"
-  );
-})(window, document);
